b9ec70b5874697ab1a07ec6fb8843cb57ef684b4
abyalax <abyalaxx@gmail.com>
2024-10-26 11:10:34 +0700

    Try Implement Framer Motion

--- src/components/UI/Skills.tsx
+++ src/components/UI/Skills.tsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import svg from "../../assets/getter";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Skills = () => {
+
+    const boxVariant = {
+        visible: { opacity: 1, scale: 1, transform: "translateY(0px)", transition: { duration: 0.7 } },
+        hidden: { opacity: 0, scale: 0, transform: "translateY(300px)" },
+      }
+      const control = useAnimation()
+      const [ref, inView] = useInView()
+    
+      useEffect(() => {
+        if (inView) {
+          control.start("visible");
+        } else {
+          control.start("hidden");
+        }
+      }, [control, inView]);
+
     const [visible, setVisible] = useState(false);
     return (
         <div className="w-full flex flex-col items-center mb-48" id="skill">
-            <div className="px-44 flex flex-col gap-8">
+            <motion.div ref={ref} variants={boxVariant} initial='hidden' animate={control} className="mt-10 px-44 flex flex-col gap-8">
                 <h2 className="text-white text-5xl my-10 mx-auto font-semibold">Tech Stack</h2>
                 <div className="flex flex-row justify-center gap-8">
                     <div className="w-44 h-44 p-3 bg-[#323131] border-4 border-[#404040] flex justify-center items-center rounded-3xl ">
@@ -63,7 +81,7 @@ const Skills = () => {
                     </>
                 )}
 
-            </div>
+            </motion.div>
         </div>
     )
 };
